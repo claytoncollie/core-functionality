ac545ede3daa7b0508c4ad6f1b7349df3673f828
Clayton Collie <clayton.collie@gmail.com>
2020-09-07 12:15:56 -0500

    Bump patch version

--- includes/class-core-functionality.php
+++ includes/class-core-functionality.php
@@ -68,7 +68,7 @@ class Core_Functionality {
 	public function __construct() {
 
 		$this->plugin_name = 'core-functionality';
-		$this->version     = '1.16.1';
+		$this->version     = '1.16.2';
 
 		$this->load_dependencies();
 		$this->set_locale();
