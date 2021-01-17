2f956fda8d97aa37d32721fe50605951f3aca9e3
Clayton Collie <clayton.collie@gmail.com>
2021-01-17 14:18:31 -0600

    Fix typos in field IDs.

--- includes/class-core-functionality.php
+++ includes/class-core-functionality.php
@@ -68,7 +68,7 @@ class Core_Functionality {
 	public function __construct() {
 
 		$this->plugin_name = 'core-functionality';
-		$this->version     = '1.18.0';
+		$this->version     = '1.18.1';
 
 		$this->load_dependencies();
 		$this->set_locale();
