7d2c871d3b47994ccc2021db08e88d07400a1b04
Clayton Collie <clayton.collie@gmail.com>
2021-08-20 17:00:24 -0500

    Unrequire the object ID to allow saving last time

--- includes/class-core-functionality.php
+++ includes/class-core-functionality.php
@@ -68,7 +68,7 @@ class Core_Functionality {
 	public function __construct() {
 
 		$this->plugin_name = 'core-functionality';
-		$this->version     = '1.18.5';
+		$this->version     = '1.18.6';
 
 		$this->load_dependencies();
 		$this->set_locale();
